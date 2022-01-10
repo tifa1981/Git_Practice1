2711513fbdf9e018b2e2a1ba06431135d970c9bb
YanchunZhou <tifa1981@gmail.com>
2022-01-10 12:28:33 -0500

    work 1 is done

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,8 @@
 public class HelloWorld {
+
     // now we need to instruct git to keep track of this project folder by enabling version control to create new local repository
+
+
+    // today is the first day of GIT
+
 }
